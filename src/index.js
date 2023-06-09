95801fe2b12a022829e49706bd4f3d23c20ef448
kenagy100@gmail.com <kenagy100@gmail.com>
2023-06-09 03:16:45 +0000

    Add index to list item

--- src/index.js
+++ src/index.js
@@ -24,9 +24,9 @@ const tasks = [
 
 const addTask = (task) => {
   list.innerHTML += `
-    <li class="task">
+    <li id=${task.index} class="task">
       <div class="task-info">
-        <input type="checkbox">
+        <input type="checkbox" ${task.completed ? 'checked' : ''}>
         <p>${task.description}</p>
       </div>
       <svg xmlns="http://www.w3.org/2000/svg" height="24" width="24"><path d="M12.025 21.15q-1 0-1.7-.7t-.7-1.675q0-1 .7-1.7t1.675-.7q1 0 1.688.7.687.7.687 1.7 0 .975-.687 1.675-.688.7-1.663.7Zm0-6.775q-1 0-1.7-.7T9.625 12q0-1 .7-1.688.7-.687 1.675-.687 1 0 1.688.687.687.688.687 1.663 0 1-.687 1.7-.688.7-1.663.7Zm0-6.75q-1 0-1.7-.713-.7-.712-.7-1.687 0-1 .7-1.688.7-.687 1.675-.687 1 0 1.688.687.687.688.687 1.688 0 .975-.687 1.687-.688.713-1.663.713Z" /></svg>
