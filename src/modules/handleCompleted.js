151422b183a6c1ab966a23f3fd7bc2397aa0d2b5
kenagy100@gmail.com <kenagy100@gmail.com>
2023-06-10 19:28:47 +0000

    Fix index

--- src/modules/handleCompleted.js
+++ src/modules/handleCompleted.js
@@ -4,7 +4,7 @@ const handleCompleted = (index) => {
   const completed = document.getElementById(`check${index}`).toggleAttribute('checked');
   const tasks = getTask();
 
-  tasks[index - 1].completed = completed;
+  tasks[(index) - 1].completed = completed;
   localStorage.setItem('data', JSON.stringify(tasks));
 };
 
