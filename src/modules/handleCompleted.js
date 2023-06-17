3c5269c63e5d523e38b6cfe3bfab80267276a3de
kenagy100@gmail.com <kenagy100@gmail.com>
2023-06-17 15:47:44 +0000

    Updated deleteCompleted function

--- src/modules/handleCompleted.js
+++ src/modules/handleCompleted.js
@@ -1,11 +1,24 @@
-import { getTask } from './storeTask.js';
+import removeTask from './removeTask.js';
+import { getTask, saveLocalStorage } from './storeTask.js';
 
 const handleCompleted = (index) => {
   const completed = document.getElementById(`check${index}`).toggleAttribute('checked');
   const tasks = getTask();
 
-  tasks[(index) - 1].completed = completed;
+  tasks[(index) - 1].complete = completed;
   localStorage.setItem('data', JSON.stringify(tasks));
 };
 
-export default handleCompleted;
+const deleteCompleted = () => {
+  const tasks = getTask();
+  tasks.forEach((task) => {
+    if (task.complete) {
+      removeTask(task.index);
+    }
+  });
+  const completed = tasks.filter((task) => !task.completed);
+  saveLocalStorage('data', JSON.stringify(completed));
+  window.location.reload();
+};
+
+export { handleCompleted, deleteCompleted };
