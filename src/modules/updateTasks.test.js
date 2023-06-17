5ce3cca50718bc6e4f29acc3b39bfaed028ea49a
kenagy100@gmail.com <kenagy100@gmail.com>
2023-06-17 15:49:14 +0000

    Add describe method to test update of description

---
+++ src/modules/updateTasks.test.js
@@ -0,0 +1,52 @@
+import { deleteCompleted, handleCompleted } from './handleCompleted.js';
+import { saveLocalStorage, updateTask } from './storeTask.js';
+// import handleCompleted from './handleCompleted.js';
+
+describe('Update task complete status and description', () => {
+  const mockObj = [
+    { index: 1, description: 'task1', complete: false },
+    { index: 2, description: 'task2', complete: false },
+    { index: 3, description: 'task3', complete: false },
+    { index: 4, description: 'task4', complete: false },
+  ];
+  saveLocalStorage('data', JSON.stringify(mockObj));
+  test('Update task description', () => {
+    document.body.innerHTML = `
+    <ul id="todo-list">
+      <li class="task" id="1">
+        <input type="text" class="task-description" value="task1">
+      </li>
+      <li class="task" id="2">
+        <input type="text" class="task-description" value="task2">
+      </li>
+      <li class="task" id="3">
+        <input type="text" class="task-description" value="task3">
+      </li>
+    </ul>
+    `;
+    const inputFields = document.querySelectorAll('.task-description');
+    const newTaskDescription = 'new task description';
+    inputFields[1].value = newTaskDescription;
+    const updatedDescription = updateTask(2, newTaskDescription);
+    expect(updatedDescription).toMatch(inputFields[1].value);
+  });
+  test('Update task completed status to true', () => {
+    document.body.innerHTML = `
+      <ul id="todo-list">
+        <li class="task" id="1">
+          <input type="checkbox" id="check1">
+        </li>
+        <li class="task" id="2">
+          <input type="checkbox" id="check2">
+        </li>
+        <li class="task" id="3">
+          <input type="checkbox" id="check3">
+        </li>
+      </ul>
+    `;
+    const checkBox = document.querySelectorAll('.task input');
+    handleCompleted(1);
+    mockObj[0].complete = true;
+    expect(checkBox[0].checked).toBe(true);
+  });
+});
